602a9549a045bae6a7b65ccff395edcf519cb263
Jon Bernhardt <bernhardt.jonathan@mayo.edu>
2018-04-19 20:57:19 -0500

    Almost complete with participation work

--- app/src/main/java/css/cis3334/fishlocatorfirebase/FishFirebaseData.java
+++ app/src/main/java/css/cis3334/fishlocatorfirebase/FishFirebaseData.java
@@ -16,10 +16,13 @@ import java.util.List;
 public class FishFirebaseData {
 
     public static final String FishDataTag = "Fish Data";
+    private DatabaseReference fishDatabase = open();
 
     public DatabaseReference open()  {
         // Get an instance of the database and a reference to the fish data in it
-
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("fish");
+        return myRef;
     }
 
     public void close() {
@@ -28,30 +31,40 @@ public class FishFirebaseData {
 
     public Fish createFish( String species, String weightInOz, String dateCaught) {           //Added String rating as a parameter
         // ---- Get a new database key for the vote
-
+        String key = fishDatabase.push().getKey();
         // ---- set up the fish object
+        Fish newFish = new Fish(key, species, weightInOz, dateCaught);
 
         // ---- write the vote to Firebase
-
+        fishDatabase.child(key).setValue(newFish);
         return newFish;
     }
 
     public Fish createFish( String species, String weightInOz, String dateCaught, String locationLatitude, String locationLongitude) {           //Added String rating as a parameter
         // ---- Get a new database key for the vote
-
+        String key = fishDatabase.push().getKey();
         // ---- set up the fish object
+        Fish newFish = new Fish(key, species, weightInOz, dateCaught, locationLatitude, locationLongitude);
 
         // ---- write the vote to Firebase
-
+        fishDatabase.child(key).setValue(newFish);
         return newFish;
     }
 
     public void deleteFish(Fish fish) {
+        String key = fish.getKey();
+        fishDatabase.child(key).removeValue();
 
     }
 
     public List<Fish> getAllFish(DataSnapshot dataSnapshot) {
+        List<Fish> fishList = null;
+        String key = "0";
+        while (fishDatabase.child(key) != null){
+            Fish fish = dataSnapshot.getValue(Fish.class);
 
+        }
+        return fishList;
     }
 
 }
